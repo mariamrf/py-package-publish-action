53cb6b623af8c0ddcd9d06d86b36d816f7600ed2
Mariam Maarouf <mariam@door2door.io>
2019-02-01 15:20:00 +0100

    Only run on

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+check_if_branch(){
+	local ref=$(jq '.ref' $GITHUB_EVENT_PATH)
+	if ! [[ -z ${BRANCH} ]]; then
+		if [ "$ref" != "\"refs/heads/$BRANCH\"" ]; then
+			echo "Not publishing because we're not on $BRANCH"
+			exit 0
+		fi
+	fi
+}
+
 go_to_build_dir(){
 	if [ ! -z $SUBDIR ]; then
 		cd $SUBDIR
@@ -18,6 +28,7 @@ upload_package(){
 	twine upload dist/*
 }
 
+check_if_branch
 go_to_build_dir
 check_if_setup_file_exists
 upload_package
