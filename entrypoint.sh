067bd92595fc4375cad82dd69132757375ec9e37
UriyaHarpeness <uriya1998@gmail.com>
2021-02-14 10:06:18 +0200

    Update pip prior to package building.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
 
-install_python_version(){
-	pyenv install $INPUT_PYTHON_VERSION
-	pyenv global $INPUT_PYTHON_VERSION
+install_python_version() {
+  pyenv install $INPUT_PYTHON_VERSION
+  pyenv global $INPUT_PYTHON_VERSION
+  if [ ! -z $INPUT_PIP_VERSION ]; then
+    pip install pip==$INPUT_PIP_VERSION
+  else
+    pip install --upgrade pip
+  fi
 }
 
-install_build_tools(){
-	pip install --upgrade setuptools wheel twine
+install_build_tools() {
+  pip install --upgrade setuptools wheel twine
 }
 
-go_to_build_dir(){
-	if [ ! -z $INPUT_SUBDIR ]; then
-		cd $INPUT_SUBDIR
-	fi
+go_to_build_dir() {
+  if [ ! -z $INPUT_SUBDIR ]; then
+    cd $INPUT_SUBDIR
+  fi
 }
 
-check_if_setup_file_exists(){
-	if [ ! -f setup.py ]; then
-		echo "setup.py must exist in the directory that is being packaged and published."
-		exit 1
-	fi
+check_if_setup_file_exists() {
+  if [ ! -f setup.py ]; then
+    echo "setup.py must exist in the directory that is being packaged and published."
+    exit 1
+  fi
 }
 
-upload_package(){
-	python setup.py sdist bdist_wheel
-	twine upload dist/*
+upload_package() {
+  python setup.py sdist bdist_wheel
+  twine upload dist/*
 }
 
 install_python_version
